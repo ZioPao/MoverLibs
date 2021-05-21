4c52dfdbe1c4e62a023b645baea1440f3a3f4677
ZioPao <picchetti1998@gmail.com>
2021-05-21 16:36:25 +0200

    some updates

--- IMUManager.cpp
+++ IMUManager.cpp
@@ -3,56 +3,93 @@
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
 // ================================================================
-volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 
-void dmpDataReady()
-{
+void dmpDataReady() {
     mpuInterrupt = true;
 }
-
 // ================================================================
 // ===                         Setup                            ===
 // ================================================================
 void IMUManager::setup(int16_t acc_x_offset, int16_t acc_y_offset, int16_t acc_z_offset, int16_t gyr_x_offset, int16_t gyr_y_offset, int16_t gyr_z_offset)
 {
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-
-    // initialize serial communication
-    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
-    // it's really up to you depending on your project)
-    Serial.begin(38400);
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setWireTimeout(3000,true); 
+	Wire.clearWireTimeoutFlag();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+#endif
 
     // initialize device
     Serial.println("Initializing I2C devices...");
     mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
 
+    
     // verify connection
     Serial.println("Testing device connections...");
     Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
+    // Manage dmp
+    uint8_t dmpStatus = mpu.dmpInitialize();
 
-    // offsets
-    mpu.setXAccelOffset(acc_x_offset);
-    mpu.setYAccelOffset(acc_y_offset);
-    mpu.setZAccelOffset(acc_z_offset);
-    
-    mpu.setXGyroOffset(gyr_x_offset);
-    mpu.setYGyroOffset(gyr_y_offset);
-    mpu.setZGyroOffset(gyr_z_offset);
+    // dmp correctly initialized
+    if (dmpStatus == 0)
+    {
+        // offsets
+        mpu.setXAccelOffset(acc_x_offset);
+        mpu.setYAccelOffset(acc_y_offset);
+        mpu.setZAccelOffset(acc_z_offset);
 
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    
+        mpu.setXGyroOffset(gyr_x_offset);
+        mpu.setYGyroOffset(gyr_y_offset);
+        mpu.setZGyroOffset(gyr_z_offset);
+
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+
+        mpu.setDMPEnabled(true);
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+    else
+    {
+        Serial.print("Error initializing!!!");
+    }
 }
 
+VectorInt16 IMUManager::getAccelValues()
+{
 
-void IMUManager::getGyroAndAccelValues(VectorInt16* acc, VectorInt16* gyr){
-    mpu.getMotion6(&acc -> x, &acc -> y, &acc -> z, &gyr -> x, &gyr -> y, &gyr -> z);
+    if (!dmpReady)
+        return;
+
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
+    {
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+
+            return aaReal;
+
+        //gyr -> x = ypr[0];
+        //gyr -> y = ypr[1];
+        //gyr -> z = ypr[2];
+
+
+        //mpu.getMotion6(&acc->x, &acc->y, &acc->z, &gyr->x, &gyr->y, &gyr->z);
+    }
 }
 
+void IMUManager::setDmpReady(bool value)
+{
+    this->dmpReady = value;
+}
--- IMUManager.h
+++ IMUManager.h
@@ -1,32 +1,36 @@
 #include "I2Cdev.h"
-#define MPU6050_INCLUDE_DMP_MOTIONAPPS20
 #include "MPU6050.h"
 #include "Wire.h"
+
 
 #define INTERRUPT_PIN 7
 
 //#define IMU_DEBUG
-
-void dmpDataReady();
-
 class IMUManager
 {
 private:
-    MPU6050 mpu;
+    MPU6050 mpu = MPU6050(0x68);
     bool dmpReady = false;  // set true if DMP init was successful
     uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
     uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
     uint16_t fifoCount;     // count of all bytes currently in FIFO
     uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-    //test stuff
-    int16_t ax, ay, az, gx, gy, gz;
-
+    // Values to get
+    VectorFloat gravity;    // [x, y, z]            gravity vector
+    Quaternion q;           // [w, x, y, z]         quaternion container
+    float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+    VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+    VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements      
 
     void MPULoading();
 
 public:
 
     void setup(int16_t acc_x_offset, int16_t acc_y_offset, int16_t acc_z_offset, int16_t gyr_x_offset, int16_t gyr_y_offset, int16_t gyr_z_offset);
-    void getGyroAndAccelValues(VectorInt16 *acc, VectorInt16 *gyr);
+    VectorInt16 getAccelValues();
+
+    void setDmpReady(bool value);
 };
+    
+    void dmpDataReady();
